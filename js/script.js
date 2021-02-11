5af9b1bc840eaa728f01f852d0888acb64e461af
Jenn Guerra <valiumisdead@yahoo.com>
2021-02-10 19:25:10 -0800

    magic8ball project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const answer = Math.floor(Math.random() * 4);
+if(answer === 0){
+    console.log('YES!');
+
+} else if(answer === 1){
+    console.log('Maybe!');
+} else if(answer === 2){
+    console.log('Ask Again!');
+} else{
+    console.log('No!');
+}
+
